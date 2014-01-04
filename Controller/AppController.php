f37f0dc4a3303b774b844d8e21deb4fa5275baf9
rbarnet <rbarnett@samford.edu>
2014-01-04 15:32:52 -0600

    Forgot to integrate the app controller

--- Controller/AppController.php
+++ Controller/AppController.php
@@ -7,9 +7,18 @@
  *
  * PHP 5
  *
+ * CakePHP(tm) : Rapid Development Framework (http://cakephp.org)
+ * Copyright (c) Cake Software Foundation, Inc. (http://cakefoundation.org)
+ *
+ * Licensed under The MIT License
+ * For full copyright and license information, please see the LICENSE.txt
+ * Redistributions of files must retain the above copyright notice.
+ *
+ * @copyright     Copyright (c) Cake Software Foundation, Inc. (http://cakefoundation.org)
  * @link          http://cakephp.org CakePHP(tm) Project
  * @package       app.Controller
  * @since         CakePHP(tm) v 0.2.9
+ * @license       http://www.opensource.org/licenses/mit-license.php MIT License
  */
 
 App::uses('Controller', 'Controller');
@@ -24,4 +33,14 @@ App::uses('Controller', 'Controller');
  * @link		http://book.cakephp.org/2.0/en/controllers.html#the-app-controller
  */
 class AppController extends Controller {
+    
+    var $menuCategories;
+    
+    public function beforeFilter() {
+        $this->loadModel('MenuCategory');
+        $this->MenuCategory->recursive = 0;
+        $this->menuCategories = $this->MenuCategory->find('all',array('conditions'=>array('MenuCategory.parent_id IS NULL'),'order'=>array('MenuCategory.order')));
+        $this->set('menuCategories', $this->menuCategories);            
+    }
+    
 }
